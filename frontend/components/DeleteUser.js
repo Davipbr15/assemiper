bdbf3c11e856d63067c26ed63e01362ed7b996da
Davipbr15 <davipbr90@gmail.com>
2022-12-07 16:43:11 -0300

    .

--- frontend/components/DeleteUser.js
+++ frontend/components/DeleteUser.js
@@ -94,7 +94,7 @@ function DeleteUser(){
                         />
                     </div>
                     <div className="mt-6">
-                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                             Checar
                         </button>
                     </div>
