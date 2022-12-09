6138fd7636a3f39f3941e1e06d4ae6f1ad1c5d31
Davipbr15 <davipbr90@gmail.com>
2022-12-09 18:52:45 -0300

    Side

--- frontend/components/LoginUser.js
+++ frontend/components/LoginUser.js
@@ -74,19 +74,28 @@ function LoginUser(){
 
 <div className="relative flex flex-col justify-center min-h-screen">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+                <h1 className="text-3xl font-semibold text-center text-purple-700">
                    Login
                 </h1>
                 <form className="mt-6" action="/loginUser" onSubmit={onSubmit} method="POST">
-                <div className="form__group field mb-2">
-                <input type="input" className="text-black form__field" 
+                    
+                    <div className="mb-2">
+                        <label
+                            htmlFor="usernameb"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Usuário
+                        </label>
+                        <input
+                            type="text"
+                            className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             name="usernameb"
                             required
                             id="usernameb"
                             onChange={onChange}
-                            placeholder="" />
-                <label for="name" className="form__label">Usuário</label>
-                </div>
+                            placeholder=""
+                        />
+                    </div>
                     <div className="mb-2">
                         <label
                             htmlFor="passwordb"
@@ -115,7 +124,7 @@ function LoginUser(){
                     </a>
                     <div className="mt-6">
                         <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                            Logas
+                            Logar
                         </button>
                     </div>
                 </form>
