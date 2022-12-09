f08b0d50bb605a65e65c4c759ea9d0b6fc0bd411
Davipbr15 <davipbr90@gmail.com>
2022-12-09 17:33:07 -0300

    tentativas falhs..

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -4,35 +4,8 @@ const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
 
   return (
+   
     <>
-      {showSidebar ? (
-        <button
-          className="flex text-4xl text-white items-center cursor-pointer fixed left-40 top-6 z-50"
-          onClick={() => setShowSidebar(!showSidebar)}
-        >
-          
-        </button>
-      ) : (
-        <svg
-          onClick={() => setShowSidebar(!showSidebar)}
-          className="fixed z-30 flex items-center cursor-pointer left-10 top-6"
-          fill="#2563EB"
-          viewBox="0 0 100 80"
-          width="40"
-          height="40"
-        >
-          <rect width="100" height="10"></rect>
-          <rect y="30" width="100" height="10"></rect>
-          <rect y="60" width="100" height="10"></rect>
-        </svg>
-      )}
-
-      <div
-        className={`top-0 left-0 w-[35vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40  ease-out-in duration-200 ${
-          showSidebar ? "translate-x-0 " : "translate-y-full"
-        }`}
-      >
-        
         <div
       className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
     >
@@ -40,7 +13,6 @@ const Sidebar = () => {
         <div className="p-2.5 mt-1 flex items-center">
           <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-assemiper"></i>
           <h1 className="font-bold text-gray-200 text-[15px] ml-3">Assemiper</h1>
-          <h1 onClick={() => setShowSidebar(!showSidebar)}>A</h1>
         </div>
         <div className="my-2 bg-gray-600 h-[1px]"></div>
       </div>
@@ -98,8 +70,8 @@ const Sidebar = () => {
         <i className="bi bi-box-arrow-in-right"></i>
         <span className="text-sm ml-4 text-gray-200 font-bold">Sair</span>
       </div>
-    </div>
-      </div>
+        </div>
+      
     </>
   );
 };
